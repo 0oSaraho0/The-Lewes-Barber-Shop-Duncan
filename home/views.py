aecc8ba7c3660611a121fd004378adceeb54756c
Sarah <0osarahtoddo0gmail.com>
2023-07-03 10:58:24 +0100

    bug fix rtrn home from add service form

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView, ListView, CreateView
 from .models import Service
 from .forms import ServiceForm
-from django.contrib.auth.mixins import LoginRequiredMixin 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # class Index(TemplateView): 
 #     template_name = 'home/index.html'
@@ -17,8 +18,8 @@ class AddService(LoginRequiredMixin, CreateView):
     model = Service
     form_class = ServiceForm
     template_name = 'home/add_service.html'
-    success_url = 'home/index.html'
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
-        # form.instance.user = self.request.user
+        
         return super(AddService, self).form_valid(form)
